17aadee8517e5c73791ad6e16ba4bfb0bd34e42b
427 Crystal <you@example.com>
2024-12-18 00:11:42 +0800

    change dataset load proc

--- Trainer/train.py
+++ Trainer/train.py
@@ -285,7 +285,7 @@ def main(args):
         args.dataset_name,
         cache_dir=args.cache_dir,
         split=args.dataset_split_name,
-        num_proc=4,
+        num_proc=2,
     )
 
     # Preprocessing the datasets.
